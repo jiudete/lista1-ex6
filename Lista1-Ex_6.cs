ad8d0b1e111b242a20db70be89a5deaccb7f3276
jiudete <130181033+jiudete@users.noreply.github.com>
2023-04-26 23:04:45 -0300

    Add files via upload

---
+++ Lista1-Ex_6.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista-Ex_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int d1;
+            int d2;
+            int média;
+
+            Console.Write("Informe o valor de d1: ");
+                d1 = int.Parse(Console.ReadLine());
+            Console.Write("Informe o valor de d2: ");
+                d2 = int.Parse(Console.ReadLine());
+            media = (d1 * d2 / 2);
+            Console.Write("a média geométrica é {0}", média);
+            
+
+
+
+
+
+        }
+    }
+}
